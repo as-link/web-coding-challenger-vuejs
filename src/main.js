acbf5da20eee99a687df655f64f44c851c5cd70c
Ed Sebti <sebti.1ahmed@gmail.com>
2018-02-11 20:24:38 +0000

    Import Vue Resource
    Import vue resource to make http calls to the api

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import Routes from './routes.js'
-
+import VueResource from 'vue-resource'
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
 
 const router = new VueRouter({
   routes: Routes,
