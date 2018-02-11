7378f2df5ebee28f20e7d21572a6f2fc1f433e8f
Ed Sebti <sebti.1ahmed@gmail.com>
2018-02-11 19:59:14 +0000

    Create home page
    Include bootstrap 4
    
    Create Home components

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="jumbotron text-center">
+    <div class="container">
+      <h1 class="jumbotron-heading">Shops Web Coding Challenge</h1>
+      <p class="lead text-muted">Something short and leading about the shops and features. Please login to view the content or register if you do not have an account.</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return{
+      title: 'Welcom'
+    }
+  }
+}
+</script>
