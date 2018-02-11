8a7c4ff8a7da5f302a3eb688b395f100dcf5eefa
Ed Sebti <sebti.1ahmed@gmail.com>
2018-02-11 20:45:56 +0000

    vuejs-paginate conponent
    Include vuejs-paginate as a dependecy
    
    Register  vuejs-paginate component

--- src/main.js
+++ src/main.js
@@ -3,14 +3,14 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import Routes from './routes.js'
 import VueResource from 'vue-resource'
-
 import Login from './components/Login.vue'
-
+import Paginate from 'vuejs-paginate'
 
 Vue.use(VueRouter);
 Vue.use(VueResource);
 
 Vue.component('login',Login);
+Vue.component('paginate', Paginate)
 
 export const bus = new Vue();
 
